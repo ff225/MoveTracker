0337b27aea6af651c1501123a9d1b7c9e30afe78
Francesco Franco <frafranco16@gmail.com>
2023-10-05 11:41:21 +0200

    modello accelerometro

---
+++ lib/data/models/accelerometer_data.dart
@@ -0,0 +1,25 @@
+class AccelerometerData {
+  final DateTime timestamp;
+  final List<double> x;
+  final List<double> y;
+  final List<double> z;
+  final int isSent;
+
+  AccelerometerData({
+    required this.timestamp,
+    required this.x,
+    required this.y,
+    required this.z,
+    this.isSent = false ? 1 : 0,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'timestamp': timestamp.toIso8601String(),
+      'x': x.map((x) => x).join(", "),
+      'y': y.map((y) => y).join(", "),
+      'z': z.map((z) => z).join(", "),
+      'isSent': isSent,
+    };
+  }
+}
