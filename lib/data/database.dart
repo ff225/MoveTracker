cd2a561556e3bc4f8d8d64a814776cf2454df94f
Francesco Franco <frafranco16@gmail.com>
2023-10-22 10:15:52 +0200

    refactor class + aggiunte tabella 'movesense_acc_data'

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,16 +2,16 @@ import 'package:move_tracker/data/models/accelerometer_data.dart';
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 
-class DatabaseAccelerometer {
+class DatabaseMoveTracker {
   static const String _databaseName = 'accelerometer.db';
   static const String _tableName = 'accelerometer_data';
+  static const String _tableNameDevice = 'movesense_acc_data';
 
-  static final DatabaseAccelerometer _database =
-      DatabaseAccelerometer._internal();
+  static final DatabaseMoveTracker _database = DatabaseMoveTracker._internal();
 
-  DatabaseAccelerometer._internal();
+  DatabaseMoveTracker._internal();
 
-  static DatabaseAccelerometer get instance => _database;
+  static DatabaseMoveTracker get instance => _database;
 
   static sql.Database? _db;
 
@@ -20,24 +20,34 @@ class DatabaseAccelerometer {
   Future<sql.Database> init() async {
     return await sql
         .openDatabase(path.join(await sql.getDatabasesPath(), _databaseName),
-            onCreate: (db, version) {
-      return db.execute('CREATE TABLE $_tableName ('
+            onCreate: (db, version) async {
+      await db.execute('CREATE TABLE $_tableName ('
           'timestamp TEXT NOT NULL,'
           'x TEXT NOT NULL,'
           'y TEXT NOT NULL,'
           'z TEXT NOT NULL,'
           'isSent INTEGER NOT NULL'
           ')');
+
+      await db.execute('CREATE TABLE $_tableNameDevice ('
+          'timestamp TEXT NOT NULL,'
+          'x TEXT NOT NULL,'
+          'y TEXT NOT NULL,'
+          'z TEXT NOT NULL,'
+          'isSent INTEGER NOT NULL'
+          ')');
+      //db.execute('CREATE TABLE $_tableNameDevice ('')');
     }, version: 1);
   }
 
   Future<void> insert(
       {required List<double> xAxis,
       required List<double> yAxis,
-      required List<double> zAxis}) async {
+      required List<double> zAxis,
+      String tableName = _tableName}) async {
     var db = await instance.database;
     await db.insert(
-        _tableName,
+        tableName,
         AccelerometerData(
           timestamp: DateTime.timestamp(),
           x: xAxis,
@@ -46,11 +56,12 @@ class DatabaseAccelerometer {
         ).toMap());
   }
 
-  Future<List<AccelerometerData>> getDataFromDB() async {
+  Future<List<AccelerometerData>> getDataFromDB(
+      {String tableName = _tableName}) async {
     var db = await instance.database;
 
     final List<Map<String, dynamic>> data =
-        await db.query(_tableName, where: 'isSent = ?', whereArgs: [0]);
+        await db.query(tableName, where: 'isSent = ?', whereArgs: [0]);
 
     return List.generate(data.length, (index) {
       return AccelerometerData(
@@ -65,10 +76,11 @@ class DatabaseAccelerometer {
     });
   }
 
-  Future<void> updateInfo(String timestamp) async {
+  Future<void> updateInfo(String timestamp,
+      {String tableName = _tableName}) async {
     var db = await instance.database;
 
-    await db.update(_tableName, {'isSent': 1},
+    await db.update(tableName, {'isSent': 1},
         where: 'timestamp = ?', whereArgs: [timestamp]);
   }
 }
