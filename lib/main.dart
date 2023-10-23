ed8fcd9081550065349bea19d89c0552b5d44a0b
Francesco Franco <frafranco16@gmail.com>
2023-10-23 16:07:54 +0200

    refactoring, aggiunto nuovo workmanager: clear-database

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
 import 'dart:async';
+import 'dart:developer';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mdsflutter/Mds.dart';
+import 'package:move_tracker/constants.dart';
 import 'package:move_tracker/data/database.dart';
 import 'package:move_tracker/screens/home_page.dart';
 import 'package:move_tracker/services/accelerometer_service.dart';
 import 'package:workmanager/workmanager.dart';
-
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await AccelerometerService().initService();
   await Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
-
-  // TODO check che la tabella non restituisca campo vuoto.
+  // TODO hardcoded movesense serial
   Workmanager().registerPeriodicTask(
-    'fromMovesenseToFirestore',
-    'send-data-acc',
+    'from-movesense-to-cloud',
+    'send-movesense-data',
     existingWorkPolicy: ExistingWorkPolicy.append,
     constraints: Constraints(
       networkType: NetworkType.connected,
@@ -29,17 +26,38 @@ Future<void> main() async {
     initialDelay: const Duration(minutes: 10),
     frequency: const Duration(minutes: 15),
   );
-  /*
+
   Workmanager().registerPeriodicTask(
-    'listenAccelerometer',
-    'send-value',
-    initialDelay: const Duration(minutes: 5),
-    frequency: const Duration(minutes: 15),
+    'from-device-to-cloud',
+    'send-device-data',
+    existingWorkPolicy: ExistingWorkPolicy.append,
     constraints: Constraints(
       networkType: NetworkType.connected,
+      //requiresDeviceIdle: true,
+      //requiresBatteryNotLow: true,
     ),
+    initialDelay: const Duration(minutes: 10),
+    frequency: const Duration(minutes: 15),
   );
-   */
+
+  Workmanager().registerPeriodicTask(
+    'from-device-to-database',
+    'save-device-data',
+    existingWorkPolicy: ExistingWorkPolicy.append,
+/*    constraints: Constraints(
+      networkType: NetworkType.connected,
+    ),*/
+    initialDelay: const Duration(minutes: 5),
+    frequency: const Duration(minutes: 15),
+  );
+
+  Workmanager().registerPeriodicTask(
+    'clear-database',
+    'clear-database',
+    frequency: const Duration(hours: 1),
+    initialDelay: const Duration(minutes: 30),
+    existingWorkPolicy: ExistingWorkPolicy.replace,
+  );
 
   runApp(
     const ProviderScope(
@@ -84,18 +102,13 @@ void callbackDispatcher() {
   Workmanager().executeTask(
     (taskName, inputData) async {
       switch (taskName) {
-        case 'task-listen':
-          AccelerometerService().service.invoke('stopService');
-          //print(hwSensor.yAxis.length);
-          print('task-listen');
-          AccelerometerService().service.startService();
+        case 'save-device-data':
+          AccelerometerService().service.invoke('saveDataToDB');
           break;
-        case 'send-value':
-          AccelerometerService().service.invoke('viewData');
+        case 'send-device-data':
           AccelerometerService().service.invoke('sendToCloud');
-          //service.startService();
           break;
-        case 'read-from-movesense':
+        case 'save-movesense-data':
           //Stop logging
           await MdsAsync.put("suunto://214530002602/Mem/DataLogger/State/",
               '''{"newState": 2}''');
@@ -104,7 +117,7 @@ void callbackDispatcher() {
           var entryId = await MdsAsync.get(
               "suunto://214530002602/Mem/Logbook/Entries/", "");
 
-          print("id: ${entryId['Content']['elements'][0]['Id']}");
+          log("id: ${entryId['Content']['elements'][0]['Id']}");
           var id = entryId['Content']['elements'][0]['Id'];
           var values = await MdsAsync.get(
               "suunto://MDS/Logbook/214530002602/byId/$id/Data", "");
@@ -113,26 +126,24 @@ void callbackDispatcher() {
           List<double> zValues = [];
           for (var accData in values['Meas']['Acc']) {
             for (var value in accData['ArrayAcc']) {
-              print(
-                  "x: ${double.parse(value['x'].toString())}, y: ${value['y']}, z: ${value['z']}");
+              log("x: ${double.parse(value['x'].toString())}, y: ${value['y']}, z: ${value['z']}");
 
               xValues.add(double.parse(value['x'].toString()));
               yValues.add(double.parse(value['y'].toString()));
               zValues.add(double.parse(value['z'].toString()));
             }
           }
-          print(
-              "x: ${xValues.length},y: ${yValues.length},z: ${zValues.length} ");
+          log("x: ${xValues.length},y: ${yValues.length},z: ${zValues.length} ");
 
           // Store values on db
-          print("store in db...");
+          log("store in db...");
           await DatabaseMoveTracker.instance.insert(
               xAxis: xValues,
               yAxis: yValues,
               zAxis: zValues,
-              tableName: "movesense_acc_data");
+              table: Constants.tableMovesenseAccelerometer);
 
-          // Delete  entry
+          // Delete entry
           await MdsAsync.del(
             "suunto://214530002602/Mem/Logbook/Entries/",
             "",
@@ -142,8 +153,10 @@ void callbackDispatcher() {
           await MdsAsync.put("suunto://214530002602/Mem/DataLogger/State/",
               '''{"newState": 3}''');
           break;
-        case 'send-data-acc':
-          await Firebase.initializeApp(
+        case 'send-movesense-data':
+          DatabaseMoveTracker.instance
+              .sendToCloud(table: Constants.tableMovesenseAccelerometer);
+          /*await Firebase.initializeApp(
             options: DefaultFirebaseOptions.currentPlatform,
           );
           var db = FirebaseFirestore.instance;
@@ -167,9 +180,14 @@ void callbackDispatcher() {
                 element.timestamp.toIso8601String(),
               ),
             );
-
-            break;
-          }
+          }*/
+          break;
+        case 'clear-database':
+          await DatabaseMoveTracker.instance
+              .deleteData(table: Constants.tableDeviceAccelerometer);
+          await DatabaseMoveTracker.instance
+              .deleteData(table: Constants.tableMovesenseAccelerometer);
+          break;
       }
       return Future.value(true);
     },
