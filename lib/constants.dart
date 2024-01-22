7430852233b839e861c17e31e7274f3b1b9dbab8
Francesco Franco <frafranco16@gmail.com>
2024-01-22 16:50:16 +0100

    aggiunto firebaseCollection

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
+  static String firebaseCollection = 'data_collected';
   static String tableDeviceAccelerometer = 'device_data';
   static String tableMovesenseAccelerometer = 'movesense_data';
   static String tableMovesenseInfo = 'movesense_info';
